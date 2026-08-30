8b2e4143269795ad804666acea7cf0568ac4014d
stageddat <hello@stageddat.dev>
2026-08-30 03:09:27 +0200

    feat: add variables and functions

---
+++ tour/main.kts
@@ -0,0 +1,19 @@
+#!/usr/bin/env kotlin
+
+// Variables
+var porros = 5; // var = variables mutables
+val coches = 7;   // val = variables read-only
+
+porros= 8;
+println(porros);
+println("Me he fumado ${porros} porros!");
+println("Me voy a fumar un porrito más :D! Ya he fumado ${porros + 1} porros!");
+println();
+
+// Funciones
+fun main() {
+    println("Hello, world!");
+    // Hello, world!
+}
+
+main();
