785044c1260cdc56aa90d693a4b20e9853d2fbac
stageddat <hello@stageddat.dev>
2026-09-05 17:36:01 +0200

    feat: add variables and types kts

---
+++ tour/01_variables_types.main.kts
@@ -0,0 +1,24 @@
+#!/usr/bin/env kotlin
+
+// Variables
+var porros = 5; // var = variables mutables
+val octopus = 7;   // val = variables read-only
+
+porros = 8;
+println(porros);
+println("Me he fumado ${porros} porros!");
+println("Me voy a fumar un porrito más :D! Ya he fumado ${porros + 1} porros!");
+println();
+
+// tipos basicos e inferencia de tipos
+// operaciones basicas: +=, -=, *=, /=, and %=
+val entero: Int = 10                     // Int
+val texto: String = "me pica el culo"    // String
+val decimal: Double = 3.14               // Double
+val numeroGrande: Long = 100_000_000L    // Long
+val booleano: Boolean = false            // Boolean
+val caracter: Char = '\n'                // Char
+
+// se requiere declarar el valor si inicias una variable
+var tetas: Int
+tetas = 3
